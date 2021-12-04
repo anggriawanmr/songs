758c0ae2b0a6364e22524f61d2cce9e09325bf85
anggriawanmr <anggriawanmr@gmail.com>
2021-12-04 13:03:17 +0700

    Finishing Displaying songs :sparkles:

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends Component {
   renderList() {
-    return this.prop.songs.map((song) => {
+    return this.props.songs.map((song) => {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button className="ui button primary" onClick={() => this.props.selectSong(song)}>
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -16,7 +19,7 @@ class SongList extends Component {
   }
 
   render() {
-    return <div className="ui divided list">{this.renderList}</div>;
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
@@ -24,4 +27,4 @@ const mapStateToProps = (state) => {
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
